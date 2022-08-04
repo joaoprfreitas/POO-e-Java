b8e51bc891d5251f675b5c355331db100db0a736
João Pedro Rodrigues <joaoprfreitas23@gmail.com>
2022-08-04 14:35:36 -0300

    movimentação do rei

--- Jogo de Xadrez/src/chess/chess/King.java
+++ Jogo de Xadrez/src/chess/chess/King.java
@@ -1,6 +1,7 @@
 package chess.chess;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,9 +16,30 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p == null || p.getColor() != getColor();
+    }
+
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position p = new Position(0, 0);
+
+        // 3x3 area
+        for (int i = -1; i < 2; i++) { // each line
+            for (int j = -1; j < 2; j++) { // each column
+                if (i == 0 && j == 0) continue;
+
+                p.setValues(position.getRow() + i, position.getColumn() + j);
+                if (getBoard().positionExists(p) && canMove(p)) {
+                    matrix[p.getRow()][p.getColumn()] = true;
+                }
+
+            }
+        }
+
         return matrix;
     }
 }
