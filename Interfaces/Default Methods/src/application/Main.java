d288221886ba5648c66924f8f76bfdfed582ae03
João Pedro Rodrigues <joaoprfreitas23@gmail.com>
2022-08-31 15:48:05 -0300

    default methods

---
+++ Interfaces/Default Methods/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import services.BrazilInterestService;
+import services.InterestService;
+import services.UsaInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        InterestService is = new UsaInterestService(1.0);
+        double payment = is.payment(amount, months);
+
+        System.out.print("Payment after " + months + " months: " + String.format("%.2f", payment));
+
+        sc.close();
+    }
+}
